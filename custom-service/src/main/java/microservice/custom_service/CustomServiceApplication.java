815d5afbc484a0780bf28becfa80002710a46bc2
Murilo Felipe <felipemurilo6@gmail.com>
2025-06-19 02:34:22 -0300

    Criando services

---
+++ custom-service/src/main/java/microservice/custom_service/CustomServiceApplication.java
@@ -0,0 +1,13 @@
+package microservice.custom_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomServiceApplication.class, args);
+	}
+
+}
